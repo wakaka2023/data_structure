97a24e3e55d791f196b51fcc1e180513c65a012f
wakaka2023 <3531764811@qq.com>
2023-09-20 21:38:42 +0800

    顺序栈

--- 栈/顺序栈例题.cpp
+++ 栈/顺序栈例题.cpp
@@ -1,8 +1,34 @@
 #include "À≥–Ú’ª.h"
+#include <string.h>
 
-int main(){
+//∆•≈‰¿®∫≈
+bool Match(char str[],int n){
     SqStack *st;
     InitStack(st);
-    Push(st,1);
-    DispStack(st);
+    for (int i=0;i<n;i++){
+        if (str[i] == '(' || str[i] == '[' || str[i] == '{')
+            Push(st,str[i]);
+        else{
+            if (StackEmpty(st))
+                return false;
+            char top;
+            Pop(st,top);
+            if (str[i] == ')' and top != '(')
+                return false;
+            if (str[i] == ']' and top != '[')
+                return false;
+            if (str[i] == '}' and top != '{')
+                return false;
+        }
+    }
+    return StackEmpty(st);
+}
+
+int main(){
+    char str[] = "[][](";
+    if (Match(str,strlen(str)))
+        printf("yes");
+    else
+        printf("false");
+    return 0;
 }
